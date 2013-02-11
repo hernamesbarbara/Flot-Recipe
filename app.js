b129a5280b88951d037bdf7c8f0646d1b447f577
Austin Ogilvie <austinogilvie@gmail.com>
2013-02-11 15:56:41 -0500

    tooltips

--- app.js
+++ app.js
@@ -10,18 +10,21 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
+function make_series() {
+    var series = [{ 
+        data: Series.create(),
+        label: "Series " + Series.randint(1,10).toString()
+      }];
+
+    return series
+};
+
 io.sockets.on('connection', function (socket) {
-    var series = [ { data: Series.create(),
-                     label: "Series " + Series.randint(1,10).toString()} ]
-    // push data immediately
-    socket.emit('plot data', series );
 
-    // push new data every 1.5 sec
+    socket.emit('plot data', make_series() );
+
+    // push new data every 2 sec
     setInterval(function(){
-        var series = [ { data: Series.create(),
-                         label: "Series " + Series.randint(1,10).toString()} ]
-        
-        socket.emit('plot data', series );
-
-    }, 1500);
+        socket.emit('plot data', make_series() );
+    }, 2000);
 });
